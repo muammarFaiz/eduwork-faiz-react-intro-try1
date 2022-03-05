1c45cd1bf4a10f92cd81857e6e9ed1b260e482b4
faizrafii <m.faiz8@yahoo.com>
2022-03-06 01:32:32 +0700

    tugas 1 edit lokal

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br />
-          Muammar Faiz Rafii
+          Muammar Faiz Rafii<br />
+          Tugas 1 edit dari lokal
         </p>
         <a
           className="App-link"
